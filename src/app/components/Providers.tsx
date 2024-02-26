ed2d4b34814ed96f2c7d602037add3493edd7ebf
Ahmad Usman <140284351+AhmadUsman2@users.noreply.github.com>
2024-02-26 16:05:30 +0500

    finished

--- src/app/components/Providers.tsx
+++ src/app/components/Providers.tsx
@@ -9,12 +9,12 @@ export default function CartProvider({ children }: { children: ReactNode }) {
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl="http://localhost:3000/stripe/success"
-      cancelUrl="http://localhost:3000/stripe/error"
+      successUrl="https://smartspend-seven-virid.vercel.app//stripe/success"
+      cancelUrl="https://smartspend-seven-virid.vercel.app//stripe/error"
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
-      language="en-US"
+      language="en-US" 
     >
       {children}
     </USCProvider>
